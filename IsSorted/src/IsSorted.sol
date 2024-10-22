a212d075b45647ba3b21e9038c0ccff4c4fc3a48
animusdeo <pieter.john.schumacher@gmail.com>
2024-10-22 19:37:41 +0200

    isSorted in assembly

--- IsSorted/src/IsSorted.sol
+++ IsSorted/src/IsSorted.sol
@@ -5,7 +5,19 @@ contract IsSorted {
     /**
      * The goal of this exercise is to return true if the members of "arr" is sorted (in ascending order) or false if its not.
      */
-    function isSorted(uint256[] calldata arr) public view returns (bool) {
-        // your code here
+    function isSorted(uint256[] calldata _arr) public pure returns (bool) {
+        assembly {
+            if lt(_arr.length, 2) {mstore(0x0, 1) return(0x0, 32)}
+
+            let prev := calldataload(_arr.offset)
+            for {let i := add(_arr.offset, 0x20)} lt(i, add(_arr.offset, mul(_arr.length, 0x20))) { i := add(i, 0x20) } {
+                let curr := calldataload(i)
+                if lt(curr, prev) {mstore(0x0, 0) return(0x0, 32)}
+                prev := curr
+            }
+
+            mstore(0x0, 1)
+            return(0x0, 32)
+        }
     }
 }
